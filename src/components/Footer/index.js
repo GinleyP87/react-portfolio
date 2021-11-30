e93d5e1a4a2323b3a5d4763d4fe36123b91ec18c
GinleyP87 <83917798+GinleyP87@users.noreply.github.com>
2021-11-30 16:30:26 -0500

    add taget=_blank to href link

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -4,8 +4,8 @@ import './style.css';
 function Footer() {
     return (
         <div id='footerDiv'>
-            <a href='https://www.linkedin.com/in/patrick-ginley-274746211/' className='footer-icon'><i className="fab fa-linkedin fa-3x"></i></a>
-            <a href='https://github.com/GinleyP87' className='footer-icon'><i className="fab fa-github-square fa-3x"></i></a>
+            <a href='https://www.linkedin.com/in/patrick-ginley-274746211/' target="_blank" className='footer-icon'><i className="fab fa-linkedin fa-3x"></i></a>
+            <a href='https://github.com/GinleyP87' target="_blank" className='footer-icon'><i className="fab fa-github-square fa-3x"></i></a>
         </div>
     )
 }
